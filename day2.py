5d60bb487ae6b5a3bbfe0cce825018908a14fec5
Andrew Patterson <saladin.zero@gmail.com>
2022-11-08 11:15:31 +0000

    Lines added

--- day2.py
+++ day2.py
@@ -1,1 +1,19 @@
-print ("Hi")
+# SET is_it_raining = INPUT "Is it raining?"
+# IF is_it_raining = "yes"
+#   THEN PRINT "You should take the car!"
+# ELSE 
+#   IF distance < 5
+#       THEN PRINT "You should walk!"
+#   ELSE 
+#       THEN PRINT "You should take the bus!"
+# END
+
+distance = 3
+is_it_raining = input("Is it raining? ")
+
+if is_it_raining == "yes":
+    print("You should take the car!")
+elif distance < 5:
+    print("You should walk!")
+else:
+    print("You should take the bus!")
